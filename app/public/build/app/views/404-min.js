c6d995daa5fc40e1a474d21d900bc1f03b395e7b
naij <naijser@gmail.com>
2017-01-06 11:53:04 +0800

    静态资源迁移出来

--- app/public/build/app/views/404-min.js
+++
@@ -1,1 +0,0 @@
-KISSY.add("app/views/404",function(e,t){return t.extend({template:'<div class=status-404> <img src="/assets/img/404.jpg"> </div>',render:function(){this.setViewHTML(this.template)}})},{requires:["mxext/view"]});
