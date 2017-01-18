c010b6585228828abbea2bd9ed5db252d18eafcf
naij <naijser@gmail.com>
2017-01-18 21:29:39 +0800

    前端升级

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,13 +7,6 @@ exports.index = function*() {
   yield this.render('index.html', config)
 }
 
-exports.debug = function*() {
-  let config = {
-    cdn: this.app.config.cdn
-  }
-  yield this.render('debug.html', config)
-}
-
 exports.notfound = function*() {
   let config = {
     cdn: this.app.config.cdn
